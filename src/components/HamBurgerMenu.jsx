1597064b5d4d6f03d6fa52776c6d5229b0a61d62
Tanmay Sahu <sahutanmay1903@gmail.com>
2024-07-21 19:57:17 +0530

    hemburger menu

--- src/components/HamBurgerMenu.jsx
+++ src/components/HamBurgerMenu.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const HamBurgerMenu = () => {
   const menu = useRef();
@@ -10,7 +11,7 @@ const HamBurgerMenu = () => {
     setOpen(!open);
   };
 
-  useEffect(() => {
+  useGSAP(() => {
     gsap.to([menu.current, menuBG.current], {
       y: open ? "0%" : "-100%",
       ease: "bounce.out",
@@ -43,7 +44,29 @@ const HamBurgerMenu = () => {
       <div
         ref={menu}
         className="h-[70vh] w-full bg-[#b5b5b5ec] z-[99] fixed top-0 left-0 right-0 bottom-0 shadow-2xl rounded-b-2xl translate-y-[-100%]"
-      ></div>
+      >
+        <div className="w-full h-full flex flex-col justify-center items-center text-6xl pt-6">
+            <a
+              href="/"
+              className="big-circle sm:text-4xl font-extrabold transition-all duration-500 font-rubikIso text-[#1d1d1dea]"
+            >
+              TANMAY
+            </a>
+          <ul className="">
+            {["HOME", "ABOUT", "PROJECTS", "CONTACT"].map((text, index) => (
+              <li key={index} className="list-none">
+                <a
+                  href={`#${text.toLowerCase()}`}
+                  className=" text-[#1d1d1d] font-bold tracking-wider text-sm sm:text-base 
+                opacity-30 transition-all duration-300 transform"
+                >
+                  {text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
